1918ab6e5358e48efcd54899a282506b48128158
zidannovrizal <zidannovrizal99@gmail.com>
2024-05-10 21:52:36 +0700

    Apply CachedImageNetwork on all of the Images

--- lib/pages/home/widgets/game_list_widget.dart
+++ lib/pages/home/widgets/game_list_widget.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:free_2_play/pages/widgets/custom_cached_image.dart';
 import 'package:free_2_play/pages/widgets/custom_loading_widget.dart';
